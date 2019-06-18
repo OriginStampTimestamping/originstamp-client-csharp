c5a53be113510894a2d9661d8ca5760e20fde87e
Daniel Muffler <daniel.muffler@uni-konstanz.de>
2019-06-18 17:43:06 +0200

    Docs update

--- src/OriginStamp.Client/Properties/AssemblyInfo.cs
+++ src/OriginStamp.Client/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("OriginStamp")]
 [assembly: AssemblyProduct("OriginStampClient")]
-[assembly: AssemblyCopyright("OriginStamp 2018")]
+[assembly: AssemblyCopyright("OriginStamp 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
